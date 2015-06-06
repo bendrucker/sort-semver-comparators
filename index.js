2dcb13f6048e7a3e0a0687ec320bf4831da86156
Ben Drucker <bvdrucker@gmail.com>
2015-06-05 20:11:05 -0400

    Coerce to strings so semver doesn't fail instanceof checks

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var semver = require('semver')
 
 module.exports = function sortSemverComparators (comparators) {
   return comparators.slice().sort(function (a, b) {
-    return semver.compare(a.semver, b.semver)
+    return semver.compare(a.semver.toString(), b.semver.toString())
   })
 }
